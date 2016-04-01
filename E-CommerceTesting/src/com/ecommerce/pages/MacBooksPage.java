3005b89a9b5171c5b235d5357a4829c40cb1574a
chaitanyak <chaitanyak@U1-DPE-CPB-0020.bitwiseglobal.net>
2016-04-01 21:05:21 +0530

    added new method

---
+++ E-CommerceTesting/src/com/ecommerce/pages/MacBooksPage.java
@@ -0,0 +1,14 @@
+package com.ecommerce.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.ecommerce.pageObjects.PageObjects;
+
+public class MacBooksPage extends PageObjects{
+
+	public MacBooksPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
